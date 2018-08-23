ea15827a33ab817efeca470397e85f872016dd9c
Félix Ibarra <felix.ibarra@ipointsystems.com>
2018-08-23 15:49:52 -0500

    Added a model.

--- quickstart-master-cli-version/curso-angular6/src/app/user/user.component.ts
+++ quickstart-master-cli-version/curso-angular6/src/app/user/user.component.ts
@@ -1,15 +1,42 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
     selector: 'user',
     templateUrl: './user.component.html'
 })
 
-export class UserComponent{
-    public nameComponent:string = 'Users';
-    public userName:string = 'Félix';
+/*export class UserComponent{
+    public componentName:string = 'Users';
+    public user:string = 'Félix';
     public age:number = 23;
-    public jobs:Array<string> = ['Ferrero Software', 'CDIS', 'Softtek', 'Inflection Point'];
+    public jobs:Array<string> = ['Microsoft', 'Google', 'Inflection Point'];
+    comodin:any;
 
-    
+    constructor(){
+        this.user = 'Félix';
+        this.age = 23;
+        this.comodin = 'Sí';
+    }
+
+    ngOnInit(){
+        this.changeName();
+    }
+
+    changeName(){
+        this.user = 'Soraya';
+    }
+}*/
+
+export class UserComponent{
+    public componentName:string = 'Users';
+    public user:User;
+
+    constructor(){
+        this.user = new User('Félix',23,'Software Engineer',true);
+    }
+
+    ngOnInit(){
+        console.log(this.user);
+    }
 }
---
+++ quickstart-master-cli-version/curso-angular6/src/app/user/user.ts
@@ -0,0 +1,17 @@
+export class User {
+    /*public name:string;
+    public age:number;
+
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }*/
+
+    /*TS*/
+    constructor(
+        public name:string,
+        public age:number,
+        public job:string,
+        public hired:boolean
+    ){}
+}
